3af56d0f6335c01d61822a53dbd6f27598256c52
feng986-eng <1093119964@qq.com>
2025-12-28 16:36:12 +0800

    comiit
    comitu一下啊

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ import os
 
 folder_path = "./photos"  # 目标文件夹路径（换成你的文件夹）
 
+老子不信了
 
 for filename in os.listdir(folder_path):
     old_path = os.path.join(folder_path, filename)
@@ -10,3 +11,4 @@ for filename in os.listdir(folder_path):
         new_path = os.path.join(folder_path, new_filename)
         os.rename(old_path, new_path)  # 重命名文件
         print(f"已重命名：{filename} -> {new_filename}")
+  print(f"已重命名：{filename} -> 无所谓{new_filename}")
